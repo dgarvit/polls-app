0c404b277c63a19c2ccf4af1c3d5442fa201a10b
Garvit <d.garvit@gmail.com>
2017-03-22 02:39:43 +0000

    adds unicode, migrations done

--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
-class Question(models.model):
+class Question(models.Model):
     question = models.CharField(max_length = 200)
-    date = models.DateTimeField('date published')
+    date = models.DateTimeField(auto_now_add = True)
+
+    def __unicode__(self):
+        return self.question
+
 
 class Choice(models.Model):
-    question = models.FoeignKey(Question)
+    question = models.ForeignKey(Question)
     choice = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
+
+    def __unicode__(self):
+        return self.choice
