2fe6d6bb8148e93dc1edb4ae165a69191a149b4d
Garvit <d.garvit@gmail.com>
2017-03-26 00:44:32 +0000

    Vote and results

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import *
-from myapp.models import Question
+from myapp.models import Question, Choice
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-date')[:5]
@@ -13,9 +14,19 @@ def detail(request, question_id):
     return render(request, 'myapp/detail.html', {'question':question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'myapp/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    p = get_object_or_404(Question, id = question_id)
+    try:
+        selected_choice = p.choice_set.get(id = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'myapp/detail.html', {
+            'question':p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('myapp:results', args=(p.id,)))
